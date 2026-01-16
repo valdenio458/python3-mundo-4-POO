be0e638e413340a172a742dc7e2c27c4f658622a
Valdenio Martinho Pereira <valdeniomartinho@gmail.com>
2026-01-16 11:03:18 -0300

    Finaliza aula-6

--- PythonProject/ex001/ex001.py
+++ PythonProject/ex001/ex001.py
@@ -1,3 +1,29 @@
 # Declaração de Classe
+class Gafanhoto:
+    def __init__(self): # Método Construtor
+        # Atributos de Instância
+        self.nome = ""
+        self.idade = 0
+
+    # Métodos de Instância
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f" {self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
 
 # Declaração de Objetos
+g1 = Gafanhoto()
+g1.nome = "Maria"
+g1.idade = 17
+print(g1.aniversario())
+print(g1.mensagem())
+
+g2 = Gafanhoto()
+g2.nome = "Mauro"
+g2.idade = 53
+g2.aniversario()
+print(g2.mensagem())
+
+g3= Gafanhoto()
+print(g3.mensagem())
