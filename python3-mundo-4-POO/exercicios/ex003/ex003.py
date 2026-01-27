243750b651a387ad7e7e9d82f0eb279a2bb12f9a
Valdenio Martinho Pereira <valdeniomartinho@gmail.com>
2026-01-27 18:54:23 -0300

    Aula 7 - ex003

---
+++ python3-mundo-4-POO/exercicios/ex003/ex003.py
@@ -0,0 +1,31 @@
+class ContaBancaria:
+    """
+   Cria uma conta bancária que permite fazer saques e depósitos
+    """
+    def __init__(self, id, nome, saldo = 0):
+        self.id= id
+        self.titular = nome
+        self.saldo = saldo
+        print(f"Conta {self.id} criada com sucesso. Saldo atual de R$ {self.saldo:.2f}")
+
+    def __str__(self):
+        return f"A conta {self.id} de {self.titular} tem R${self.saldo:.2f} de saldo."
+
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f"Depósito de R${valor:.2f} autorizado na conta {self.id}")
+
+    def sacar(self, valor):
+        if valor > self.saldo:
+            print(f"Saque de R$ {valor:.2f} negado na conta {self.id} SALDO INSUFICIENTE")
+        else:
+          self.saldo -= valor
+          print(f"Saque de R${valor:.2f} autorizado na conta {self.id}")
+
+
+
+c1 = ContaBancaria(112, 'Gustavo', 3000)
+# print(c1.__doc__)
+c1.depositar(500)
+c1.sacar(5000)
+print(c1)
